4baacc0628243518aaf626a25112a3aabcfd051b
nilinswap <nilinswap@gmail.com>
2017-02-26 22:28:54 +0530

    Add all files for Q1 Q2

---
+++ boy.py
@@ -0,0 +1,9 @@
+class boy:
+	
+	def __init__(self,name,clvrn,sxn,minhotn,budget):
+		self.name=name
+		self.clvrn=clvrn#boy's cleverness: a no. from 0-9
+		self.sxn=sxn#boy's attractivenss: a no. from 0-9
+		self.minhotn=minhotn#boy's minimum requirement of hotness: a no. from 0-9
+		self.budget=budget#boy's budget: a no. from 0-999
+	
