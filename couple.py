2fb5486f4dbbab879dcf8f11c003e8c16c0d2e6e
nilinswap <nilinswap@gmail.com>
2017-04-10 02:55:56 +0530

    Delete couple.py

--- couple.py
+++
@@ -1,7 +0,0 @@
-import math
-class couple:
-	def __init__(self,bo,gir):
-		self.bo=bo
-		self.gir=gir
-		self.hpns=bo.hpns+gir.hpns
-		self.cmpblty=bo.budget-gir.mntcst+math.fabs(bo.clvrn-gir.smrtn)+math.fabs(bo.sxn-gir.hotn)#these two are added attributes of couple object
